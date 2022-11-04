3e65e343ea4490a0ede6382b2e788210b5ab7e7f
Adam Perlin <adamp@nanosoft.com>
2022-11-03 19:21:58 -0700

    Progress from meeting

--- axioms.py
+++ axioms.py
@@ -9,7 +9,9 @@ Policy gives access to actions on identities and resources
 Groups are made of multiple users
 
 Resources can also own policies
-
+Policies give access to principals
+Principals describe identities and resources
+Every identity has an associated IAM string
 
 Policy Evaluation:
 - Decision  starts with deny
@@ -26,49 +28,55 @@ Policy Evaluation:
                 [Session principles in  the future]
 '''
 
-PrincipleSort = z3.DeclareSort("Principle")
+PrincipalSort = z3.StringSort()
 
-
+Policy = z3.Datatype("Policy")
+Policy.declare("Policy",("Effect", z3.BoolSort()), ("Principle", PrincipalSort))
+Policy.declare("Nothing")
 
 Role = z3.Datatype("Role")
-Policy = z3.Datatype("Policy")
-Policy.declare("Policy",("Effect", z3.BoolSort()),("Principle", Role))
-
-Role.declare("Role",("policies",Policy))
+Role.declare("Role",("policy",Policy), ("iam", z3.StringSort()))
 
 Policy, Role = z3.CreateDatatypes(Policy,Role)
 
+# Allow
+Allow = z3.Function('allow', Role, Role, z3.BoolSort())
 
-IdentitySort = z3.DeclareSort("Identity")
-Allow = z3.Function('allow',IdentitySort,IdentitySort,z3.BoolSort())
+# User = z3.Datatype("User")
+# User.declare("User",("policies",PoliciesSort))
+# User = User.create()
 
+p = z3.Const('p', Policy)
+i = z3.Const('i', Role)
+#check_policy = z3.Lambda([p,i], z3.And(Policy.Effect(p), Role.iam(i) == Policy.Principle(p)))
 
+def check_policy(p, i):
+	return z3.And(Policy.Effect(p), Role.iam(i) == Policy.Principle(p))
 
+def checkUser(role, identity):
+	return check_policy(Role.policy(role), identity)
 
+test_policy = Policy.Policy(z3.BoolVal(True), z3.StringVal("C"))
+test_role = Role.Role(test_policy, z3.StringVal("B"))
+access_role1 = Role.Role(
+				Policy.Policy(z3.BoolVal(True), z3.StringVal("D")), 
+				z3.StringVal("C"))
+access_role2 = Role.Role(Policy.Nothing, z3.StringVal("D"))
 
-User = z3.Datatype("User")
-User.declare("User",("policies",PoliciesSort))
-User = User.create()
+r1 = z3.Const('r1', Role)
+r2 = z3.Const('r2', Role)
+r3 = z3.Const('r3', Role)
+allowsImplication = z3.ForAll([r1, r2], z3.Implies(checkUser(r1, r2), Allow(r1, r2)))
+transitivity = z3.ForAll([r1, r2, r3], z3.Implies(z3.And(Allow(r1, r2), Allow(r2, r3)), Allow(r1, r3)))
 
-p = Policy
-i = IdentitySort
-# check_policy = z3.Lambda([p,i],z3.And(Policy.Effect(p),i==Policy.Principle(p)))
+s = z3.Solver()
+s.add(allowsImplication)
+s.add(transitivity)
+s.add(z3.And(r1 == test_role, r2 == access_role1, r3 == access_role2)) 
+s.add(checkUser(r1, r2))
+s.add(checkUser(r1, r3))
 
-# def checkUser(user,identity):
-#     return z3.Exists(i,check_policy(User.polcies(user)[i],identity))
-
-
-pols = z3.Array('pols',z3.IntSort(),Policy)
-a = Role.Role(pols)
-p = Policy.Policy(z3.BoolVal(True),a)
-pols[0] = p
-b = Role.Role(pols)
-solver = z3.Solver()
-# solver.add(checkUser(b,a))
-solver.add(Policy.Effect(z3.Select(pols,0)))
-assert solver.check() == z3.sat
-
-
-
-
-
+if s.check() == z3.sat:
+	print(s.model())
+else:
+	print('unsat')
