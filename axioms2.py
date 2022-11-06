3c79e60a19d6d32a8331333708177e4eb8fda6b7
Mihir <mihir1003@gmail.com>
2022-11-06 14:13:26 -0800

    Seperation of identity and principal

--- axioms2.py
+++ axioms2.py
@@ -1,34 +1,58 @@
 from z3 import *
 
+
 I = DeclareSort("I")
 P = DeclareSort("P")
-Policy = Function("Policy",BoolSort(),I,I,BoolSort())
+Policy = Function("Policy",BoolSort(),I,P,BoolSort())
 
+AsPrincipal = Function("AsPrinciple",I,P,BoolSort())
 
+DeclareIdentity = Function("DeclareIdentity",I,BoolSort())
+Identity = Function("Identity",StringSort(),I)
+Principal = Function("Principal",StringSort(),P)
+Allowed = Function("Allow", I, P, BoolSort())
 Allow = Function("Allow", I, I, BoolSort())
 tc_allow = TransitiveClosure(Allow)
-role1,role2,role3,role4 = Consts("role1 role2 role3 role4",I)
-i1,i2 = Consts("i1 i2",I)
+role1,role2,role3,role4 = map(lambda x :  StringVal(x),["role1","role2", "role3", "role4"])
+
+# def asPrinciple() :
+
+
+# i1,i2 = Consts("i1 i2",I)
+# p1,p2 = Consts("p1 p2",P)
+# print(p1.,p2)
+
+i1,i2 = Strings("i1 i2")
+p1,p2 = Strings("p1 p2")
+print(i1,i2)
 true = BoolVal(True)
 false = BoolVal(False)
 axioms =[
-    # ForAll([i1,i2],Allow(i1,i2)==BoolVal(False)),
-    ForAll([i1,i2],Implies(Policy(BoolVal(True),i1,i2),Allow(i1,i2))),
-# ForAll([i1,i2],Implies(Policy(BoolVal(False),i1,i2),Allow(i1,i2)==BoolVal(False))),
-# ForAll([i1,i2],If(Policy(true,i1,i2),Allow(i1,i2)==BoolVal(True),Allow(i1,i2)==BoolVal(False))),
-# Policy(BoolVal(True),role2,role1),
-# Policy(BoolVal(True),role1,role3),
-# Policy(BoolVal(True),role3,role4)
-Policy(true,role1,role2),
-Policy(true,role2,role3)
+     ForAll([i1,p1],Implies(i1==p1,AsPrincipal(Identity(i1),Principal(p1)))),
+    ForAll([i1,p1],Implies(
+            And(Policy(BoolVal(True),Identity(i1),Principal(p1))),
+            # AsPrincipal(Identity(i1),Principal(p1))),
+        Allowed(Identity(i1),Principal(p1)))),
+    ForAll([i1,p1,i2],Implies(And(
+        AsPrincipal(Identity(i1),Principal(p1)),
+        Allowed(Identity(i2),Principal(p1))),
+        Allow(Identity(i2),Identity(i1)))),
+    Policy(true,Identity(role1),Principal(role2)),
+    Policy(true,Identity(role2),Principal(role3)),
+    Policy(true,Identity(role3),Principal(role3)),
+
+    # DeclareIdentity(Identity(role4))
+    # DeclareIdentity(Identity(role3))
 ]
 
 s = Solver()
 s.add(axioms)
 print(s.check())
-s.add(Not(tc_allow(role1,role2)))
-if s.check() == "unsat":
+# s.add(Not(Allowed(Identity(role1),Principal(role2))))
+# s.add(Not(AsPrincipal(Identity(role1),Principal(role2))))
+s.add(Not(tc_allow(Identity(role1),Identity(role3))))
+if s.check() == unsat:
     print("authorized")
 else:
     print("unauthourized")
-# print(s.model())
+    print(s.model())
